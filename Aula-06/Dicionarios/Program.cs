c1bb31f5e7a48cabe6058f41e0abd1ac394597e1
liviaalarcon <livia.a.santos7@edu.senai.br>
2026-08-12 11:15:22 -0300

    Aula-06 - Colletions em C#

---
+++ Aula-06/Dicionarios/Program.cs
@@ -0,0 +1,65 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("\n=== TRABALHANDO COM DICIOÁRIO ===\n");
+        /* Sintaxe Dictionary<chave, valor>
+                             <key, value> */
+
+        // 1 - Criar um dicionário vazio
+        Dictionary <string, string> dicionario = new Dictionary <string, string>();
+
+        // 2 - Adicionar Elementos no dicionário
+        dicionario.Add("Casa","Construção destinada à moradia.");
+        dicionario.Add("Livro","Conjunto de páginas encadernadas contendo textos, imagens ou informações.");
+        dicionario.Add("Amizade","Relação de afeto, confiança e companheirismo entre pessoas.");
+        dicionario.Add("Escola","Instituição destinada ao ensino e à educação de crianças, jovens ou adultos.");
+
+        // 3 - Percorrer todos os Elementos de um Dicionário
+        Console.WriteLine();
+        foreach(var item in dicionario)
+        {
+            Console.WriteLine($"{item.Key} - {item.Value}");
+        }
+
+        // 4 - Alterar um Elemento do dicionário
+        dicionario["Casa"] = "Construção destinada a moradia definitiva ou temporária.";
+
+        Console.WriteLine("\n=== DICIOÁRIO ATUALIZADO ===");
+        Console.WriteLine();
+        foreach(var item in dicionario)
+        {
+            Console.WriteLine($"{item.Key} - {item.Value}");
+        }
+
+        // 5 - Verificar se uma chave existe dentor do Dicionário
+        string termo;
+        bool chaveExiste = dicionario.TryGetValue("Amizade", out termo);
+        Console.WriteLine();
+        if (chaveExiste)
+        {
+            Console.WriteLine($"A palavra encontrada: {termo}");
+        }
+        else
+        {
+            Console.WriteLine("A palavra não foi encontrada no Dicionário");
+        }
+
+        // 6 - Excluir um elemento do dicionário -> Somente por Chave(key)
+        dicionario.Remove("Casa");
+        Console.WriteLine();
+        foreach(var item in dicionario)
+        {
+            Console.WriteLine($"{item.Key} - {item.Value}");
+        }
+
+        // 7 - Ordenar um Dicionário por chave
+        var DicionarioOrdenado = dicionario.OrderBy(i => i.Key);
+        Console.WriteLine();
+        foreach(var item in DicionarioOrdenado)
+        {
+            Console.WriteLine($"{item.Key} - {item.Value}");
+        }
+
+    }
+}
---
+++ Aula-06/Filas/Program.cs
@@ -0,0 +1,49 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("=== TRABALHANDO COM FILAS (FIFO)===");
+
+        // 1 - Criar uma nova fila vazia
+        Queue<string> filaBanco = new Queue<string>();
+
+        // 2 - Adicionar elementos na fila
+        filaBanco.Enqueue("Lívia");          // 0
+        filaBanco.Enqueue("Simone");         // 1
+        filaBanco.Enqueue("Isac");         // 2
+        filaBanco.Enqueue("Cacau");      // 3
+
+        // 3 - Percorrer elementos de uma fila 
+        foreach (var pessoa in filaBanco)
+        {
+            Console.WriteLine(pessoa);
+        }
+        
+        // 4 - Retirar um elemento da lista
+        string pessoaRetirada = filaBanco.Dequeue();
+        Console.WriteLine();
+        Console.WriteLine($"O Nome chamado foi: {pessoaRetirada}");
+
+        filaBanco.Enqueue("Lucrécia");
+
+        Console.WriteLine();
+
+        foreach(var pessoa in filaBanco)
+        {
+            Console.WriteLine(pessoa);
+        }
+
+        // 5 - Verificar se existe um determinado elemnto na lista
+        string procurar = "Isac";
+        bool achou = filaBanco.Contains(procurar);
+        Console.WriteLine();
+        if (achou)
+        {
+            Console.WriteLine($"{procurar} está na fila!");
+        }
+        else
+        {
+            Console.WriteLine($"{procurar} NÃO está na fila!");
+        }
+    }
+}
